64b9b244e9cfbe276200cb188dbea2901a904a2c
Iury <iurymacali0101@hotmail.com>
2023-03-29 09:00:53 -0300

    add the map to the listing page using leaflet

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -23,6 +23,7 @@ import {
 } from 'react-icons/fa';
 import { getAuth } from 'firebase/auth';
 import Contact from '../components/Contact';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 export default function Listing() {
   const auth = getAuth();
@@ -139,7 +140,7 @@ export default function Listing() {
             </li>
           </ul>
           {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
-            <div className="mt-6">
+            <div className="mt-6 mb-6">
               <button
                 onClick={() => {
                   setContactLandlord(true);
@@ -154,7 +155,27 @@ export default function Listing() {
             <Contact userRef={listing.userRef} listing={listing} />
           )}
         </div>
-        <div className="bg-green-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
+        <div className="w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden md:ml-2">
+          <MapContainer
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: '100%', width: '100%' }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+        {console.log(listing.geolocation.lng)}
       </div>
     </main>
   );
